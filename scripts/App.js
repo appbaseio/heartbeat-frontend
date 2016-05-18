b3a5f453004d95a2cf296f2021a6f22e20f78fc8
Karan <karan.p.vyas.2012@gmail.com>
2016-05-18 15:14:31 +0530

    cleaned up

--- scripts/App.js
+++ scripts/App.js
@@ -3,17 +3,14 @@ import TextField from 'material-ui/TextField';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-// import AwesomeForm from './awesomeForm.js';
 import GetHeaders from './getHeaders.js';
 import GetParams from './getParams.js';
 import GetBody from './getBody.js';
 import GetAuthDetails from './getAuthDetails.js';
 import MethodBox from './methodBox.js';
 
-
 export default class App extends Component {
 
-    // getInitialState()
     state = {
         restApiUrl : "",
         data : "",
@@ -25,19 +22,15 @@ export default class App extends Component {
         var temp = this.state;
         temp.restApiUrl = e.target.value;
         this.setState(temp);
-        // console.log(this.state);
     };
 
-    streamAndUpdate = (responseObj) => {
-        console.log("now streaming ka try");
-        //TODO add the script tag somehow from here itself
+    streamAndUpdate = (type) => {
         var config = {
             appname: 'jsfiddle-demo',
             username: '7eJWHfD4P',
             password: '431d9cea-5219-4dfb-b798-f897f3a02665',
-            type: responseObj.type
+            type: type
         };
-
         var appbaseRef = new Appbase({
             url: 'https://scalr.api.appbase.io',
             appname: config.appname,
@@ -54,15 +47,14 @@ export default class App extends Component {
             }
         };
 
+        //to use inside the callback of searchStream
         var a = this.state;
         var f = this.setState;
-        // console.log(a);
         var self = this;
+
         appbaseRef.searchStream(requestObject).on('data', function(stream) {
-        //  console.log("<br>New streaming object: ", JSON.stringify(stream))
-            // console.log(a);
+            //displaying the updated json data
             var temp = self.state;
-            // console.log(temp);
             temp.data = JSON.stringify(stream._source);
             temp.changedNum = temp.changedNum + 1;
             self.setState(temp);
@@ -73,26 +65,43 @@ export default class App extends Component {
     };
 
     submitAndGetType = () => {
-        var objectToSend = {
+        var currentTime = new Date().getTime().toString();
+        var objectToIndex = {
+            restApiUrl : this.state.restApiUrl,
+            type : currentTime,
             headers : this.refs.headers.state,
             body : this.refs.body.state,
             params : this.refs.params.state,
             authDetails : this.refs.authDetails.state,
             method : this.refs.method.state.method
-        }
-        var finalWala = this.streamAndUpdate;
-        var serverUrl = "http://localhost:3000/someRouteAuth?url=";
-        var restApiUrl = this.state.restApiUrl;
-        var streamAndUpdate = this.streamAndUpdate; // TODO -  any better way?
-        var jqxhr = $.post(serverUrl+restApiUrl, objectToSend , function(data){
-            //console.log(data);//TODO-- this is Tricky.
-        }).done(function(data){
-            streamAndUpdate(data);
+        };
+        var config = {
+            appname: 'jsfiddle-demo',
+            username: '7eJWHfD4P',
+            password: '431d9cea-5219-4dfb-b798-f897f3a02665',
+            type: "pollRequests"
+        };
+        var appbaseRef = new Appbase({
+            url: 'https://scalr.api.appbase.io',
+            appname: config.appname,
+            username: config.username,
+            password: config.password
+        });
+        var requestObject = {
+            type: config.type,
+            body: objectToIndex,
+            id: currentTime,
+        };
+        var streamAndUpdate = this.streamAndUpdate;
+        appbaseRef.index(requestObject).on('data', function(response) {
+            console.log("successfully indexed.");
+            streamAndUpdate(currentTime);
+        }).on('error', function(error) {
+            console.log("error in indexing.");
         });
     };
 
     render() {
-        // var a = this.handleUrlChange;
         return (
             <div>
                 <MuiThemeProvider muiTheme={getMuiTheme()}>
@@ -120,12 +129,11 @@ export default class App extends Component {
                 </div>
 
                 <div>
-                    new things -- <br /> TODO -- //delete this line <br />
+                    <br /><br />
                     <MethodBox ref="method"/>
                     <GetHeaders ref="headers" />
                     <GetParams ref="params" />
                     <GetBody ref="body" />
-
                 </div>
 
                 <div>
