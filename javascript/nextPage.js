59f28d32b23f15eef4b7dc013e8ebe7607a417c2
Karan <karan.p.vyas.2012@gmail.com>
2016-05-25 18:24:12 +0530

    frontend awesome v2, with auth.

---
+++ javascript/nextPage.js
@@ -0,0 +1,25 @@
+// set ajax
+$.ajaxSetup({
+    crossDomain: true,
+    xhrFields: {
+        withCredentials: true
+    }
+});
+
+// check if user is already logged or not
+$.ajax({
+    type: 'GET',
+    async: false,
+    url: 'https://accapi.appbase.io/user',
+    dataType: 'json',
+    contentType: "application/json",
+    success: function(full_data) {
+        console.log(full_data);
+        isLoggedIn = true;
+    },
+    error: function(e) {
+        console.log(e);
+        console.log('unsucesseful');
+        window.location.href = 'index.html';
+    }
+});
