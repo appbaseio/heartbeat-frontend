d79756f52d869aab63bc66df9bbb022fb3ffe371
Karan <karan.p.vyas.2012@gmail.com>
2016-05-27 16:15:46 +0530

    changed path from localhost to methi.io's name

--- javascript/nextPage.js
+++ javascript/nextPage.js
@@ -20,6 +20,6 @@ $.ajax({
     error: function(e) {
         console.log(e);
         console.log('unsucesseful');
-        window.location.href = 'index.html';
+        window.location.href = 'http://methi.io/REST-API-to-Streaming-API-frontend/index.html';
     }
 });
