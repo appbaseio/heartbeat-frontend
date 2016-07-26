39e1c12a0b8ad8d33fc7c85ba2ae1ee0930f4d77
karan <karan.p.vyas.2012@gmail.com>
2016-07-26 07:11:30 +0530

    corrected intercom json created_at

--- javascript/nextPage.js
+++ javascript/nextPage.js
@@ -27,7 +27,7 @@ $.ajax({
             app_id: "jnzcgdd7",
             name: full_data.body.details.name,
             email: full_data.body.details.email,
-            created_at: new Date("2016-07-22T04:37:38.629342Z").getTime(),
+            created_at: parseInt(new Date("2016-07-22T04:37:38.629342Z").getTime()/1000),
     	};
         var codeToAdd = "<script>window.intercomSettings ="+JSON.stringify(userObj)+"</script>"+ "<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/jnzcgdd7';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>";
         postscribe(document.getElementsByTagName('head')[0],codeToAdd);
