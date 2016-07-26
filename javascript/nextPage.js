fd5e4c0d86f803a4177f62252d74d23b53f58bbb
karan <karan.p.vyas.2012@gmail.com>
2016-07-26 05:37:26 +0530

    intercom added

--- javascript/nextPage.js
+++ javascript/nextPage.js
@@ -14,7 +14,14 @@ $.ajax({
     dataType: 'json',
     contentType: "application/json",
     success: function(full_data) {
+        console.log("made accapi call from nextPage.js");
         console.log(full_data);
+        localStorage.setItem('intercomSettings', JSON.stringify({
+            app_id: "jnzcgdd7",
+            name: full_data.body.details.name,
+            email: full_data.body.details.email,
+            created_at: new Date("2016-07-22T04:37:38.629342Z").getTime()
+        }));
         isLoggedIn = true;
     },
     error: function(e) {
